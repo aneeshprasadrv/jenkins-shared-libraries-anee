99b87871bad2838bdfb895fb3f8b2395ce9fdc94
Aneesh Prasad <87414831+aneeshprasadrv@users.noreply.github.com>
2025-07-24 14:52:19 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call(String ProjectName, String imageTag, String Dockerhubuser){
   sh 'whoami'
-  sh 'docker build -t "${Dockerhubuser}/${ProjectName}:${imageTag}' ."
+  sh 'docker build -t "${Dockerhubuser}/${ProjectName}:${imageTag} ."
 }
